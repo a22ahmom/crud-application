e4b7d008aeb2ab77596f28d1720e49a8f46467ed
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-19 10:54:42 +0200

    again: created an array to store the books and a method to add a new book in the array.

--- src/app/book-service.ts
+++ src/app/book-service.ts
@@ -3,4 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class BookService {}
+export class BookService {
+
+  books: any[] = [];
+
+  addBook(book: any){
+    this.books.push(book);
+  }
+}
