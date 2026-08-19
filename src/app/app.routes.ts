ef7683530e287c4a7bc68b38556c93bffeea0534
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-19 10:14:02 +0200

    configured the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AddBook } from './add-book/add-book';
+import { BookList } from './book-list/book-list';
 
 export const routes: Routes = [
     {
         path: 'books/new',
         component: AddBook
+    },
+    {
+        path: 'books',
+        component: BookList
     }
 ];
