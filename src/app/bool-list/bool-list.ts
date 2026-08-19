bc8fdf37f86954a57edd6da1afe79a4948789927
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-19 10:00:18 +0200

    imported routerLink and migrated code from app.html

--- src/app/bool-list/bool-list.ts
+++ src/app/bool-list/bool-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-bool-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './bool-list.html',
   styleUrl: './bool-list.css',
 })
