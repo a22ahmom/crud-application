4b453cd3e4c365a1594f9a8cc7867c06e99e6ce1
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-19 11:06:27 +0200

    added code to create a book object and to add it to the array when the user clicks "skapa"

--- src/app/add-book/add-book.ts
+++ src/app/add-book/add-book.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BookService } from '../book-service';
 
 @Component({
   selector: 'app-add-book',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './add-book.html',
   styleUrl: './add-book.css',
 })
-export class AddBook {}
+export class AddBook {
+
+  title = '';
+  author = '';
+  publicationDate = '';
+
+  constructor (
+    private bookService: BookService,
+    private router: Router
+  ) {}
+
+  addBook(){
+
+    const newBook = {
+      title: this.title,
+      author: this.author,
+      publicationDate: this.publicationDate
+    };
+
+    this.bookService.addBook(newBook);
+
+    this.router.navigate(['/books']);
+  }
+}
