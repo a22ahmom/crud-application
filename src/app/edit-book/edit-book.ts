db37b85344a29acf2b654d49342be0b7445eba34
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-20 09:47:53 +0200

    implemented code to read the book id from the URL

--- src/app/edit-book/edit-book.ts
+++ src/app/edit-book/edit-book.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from '../book-service';
 
 @Component({
   selector: 'app-edit-book',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './edit-book.html',
   styleUrl: './edit-book.css',
 })
-export class EditBook {}
+export class EditBook {
+
+  bookIndex: number;
+
+  title = '';
+  author = '';
+  publicationDate = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private bookService: BookService
+  ){
+
+    this.bookIndex = Number(
+      this.route.snapshot.paramMap.get('id')
+    );
+
+    const book = this.bookService.books[this.bookIndex];
+
+    if (book) {
+      this.title = book.title;
+      this.author = book.author;
+      this.publicationDate = book.publicationDate;
+    }
+  }
+}
