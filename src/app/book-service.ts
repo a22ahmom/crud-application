2deb4caf80801f020e9231b8124de16253272443
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-20 10:12:54 +0200

    updated the service to include the Book type

--- src/app/book-service.ts
+++ src/app/book-service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Book } from './models/book';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
 
-  books: any[] = [];
+  books: Book[] = [];
 
-  addBook(book: any){
+  addBook(book: Book){
     this.books.push(book);
   }
 
-  updateBook(index: number, updatedBook: any) {
-    this.books[index] = updatedBook;
+  updateBook(id: number, updatedBook: Book) {
+    const index = this.books.findIndex(book => book.id === id);
+
+    if (index !== -1){
+      this.books[index] = updatedBook;
+    }
   }
 }
