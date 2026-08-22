480f6b03e4cb17049276666a088623e225aafca4
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-22 12:16:53 +0200

    added entityframeworkcore, and other important packages for database handling. Also added a class to be the link between c# and database.

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users { get; set; }
+}
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e241e8cf609d109d78df04eee793ab68e2726c81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0334d029b0e06ab9bb82218b6942ab783b0fb2e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
