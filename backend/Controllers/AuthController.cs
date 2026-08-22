3935ca9a746fbcd28592bf60247ef53483a8c8d6
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-22 19:29:38 +0200

    fixed namespace issue and created register endpoint

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using backend.Data;
 using backend.DTOs;
 using backend.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace backend.Controllers;
 
@@ -15,5 +16,34 @@ public class AuthController : ControllerBase
     public AuthController(AppDbContext context)
     {
         _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterDtos request)
+    {
+        var userExists = await _context.Users
+            .AnyAsync(user => user.Username == request.Username);
+
+        if (userExists)
+        {
+            return BadRequest("Användarnamnet finns redan.");
+        }
+
+        var user = new User
+        {
+            Username = request.Username
+        };
+
+        var passwordHasher = new PasswordHasher<User>();
+
+        user.PasswordHash = passwordHasher.HashPassword(
+            user,
+            request.Password
+        );
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return Ok("Användaren har registrerats");
     }
 }
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,7 +1,7 @@
 namespace backend.Models;
 
-public class User{
-
+public class User
+{
     public int Id { get; set; }
 
     public string Username { get; set; } = string.Empty;
