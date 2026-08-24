5e805cc02f4d5e4e42c66de37ad6cff2c5b480ac
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-24 17:59:44 +0200

    implemented code for the register page

--- frontend/src/app/register/register.ts
+++ frontend/src/app/register/register.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../services/auth';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+
+  username = '';
+  password = '';
+
+  constructor(
+    private authService: Auth,
+    private router: Router
+  ){}
+
+  register() {
+    this.authService
+      .register(this.username, this.password)
+      .subscribe({
+        next: response => {
+          console.log(response);
+
+          this.router.navigate(['/login']);
+        },
+
+        error: error => {
+          console.error('Registreringen misslyckades:', error);
+        }
+      });
+  }
+}
