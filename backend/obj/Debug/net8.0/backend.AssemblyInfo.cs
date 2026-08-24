f8e5472f2ae46c7401ae3e4236afec90bb776149
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-24 21:35:28 +0200

    added a new user to test the interceptor

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082fe9e962628681b724e0806af348d50eb0bc91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3ee9e7c07b1ea58d0aec15e8f4ad264c026f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
