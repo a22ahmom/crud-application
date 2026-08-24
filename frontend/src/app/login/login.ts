1a661733db6a6a5179048609ab56d9b6fabf78f9
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-24 19:14:05 +0200

    implemented code in login

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Auth } from '../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  username = '';
+  password = '';
+
+  constructor(
+    private authService: Auth,
+    private router: Router
+  ){}
+
+  login() {
+    this.authService
+      .login(this.username, this.password)
+      .subscribe({
+        next: response => {
+          console.log('JWT:', response.token);
+
+          localStorage.setItem(
+            'token',
+            response.token
+          );
+
+          this.router.navigate(['/books']);
+        },
+
+        error: error => {
+          console.error('Inloggningen misslyckades:', error);
+        }
+      });
+  }
+
+}
