7a25f2f0f126ec43e97d515c4e280856e1da637d
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-24 16:21:27 +0200

    created a bookscontroller class

---
+++ backend/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public BooksController(AppDbContext context)
+    {
+        _context = context;
+    }
+}
