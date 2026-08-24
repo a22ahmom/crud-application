b0cf87e74dcd094c00579d6ec64ba924e5b3d580
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-24 21:58:25 +0200

    changed booklist a little bit

--- frontend/src/app/book-list/book-list.ts
+++ frontend/src/app/book-list/book-list.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BookService } from '../book-service';
+import { Book } from '../models/book';
 
 @Component({
   selector: 'app-book-list',
@@ -8,9 +9,27 @@ import { BookService } from '../book-service';
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
-export class BookList {
+export class BookList implements OnInit{
 
-  constructor(public bookService: BookService){}
+  books: Book[] = [];
+
+  constructor(private bookService: BookService){}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getBooks().subscribe({
+      next: books => {
+        this.books = books;
+      },
+
+      error: error => {
+        console.error('Kunde inte hämta böcker:', error);
+      }
+    });
+  }
 
   deleteBook(id: number) {
     this.bookService.deleteBook(id);
