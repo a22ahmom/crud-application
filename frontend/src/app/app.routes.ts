25475fda6c9cca65055a9d1d68f1050394f10754
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-24 19:22:23 +0200

    added routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,20 +2,30 @@ import { Routes } from '@angular/router';
 import { AddBook } from './add-book/add-book';
 import { BookList } from './book-list/book-list';
 import { EditBook } from './edit-book/edit-book';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'books',
+        redirectTo: 'login',
         pathMatch: 'full'
     },
     {
-        path: 'books/new',
-        component: AddBook
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'register',
+        component: Register
     },
     {
         path: 'books',
         component: BookList
+    },
+    {
+        path: 'books/new',
+        component: AddBook
     },
     {
         path: 'books/edit/:id',
