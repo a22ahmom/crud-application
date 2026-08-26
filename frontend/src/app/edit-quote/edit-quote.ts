22b02d5cd600714d96ae559eedeb64347f1fbd2e
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-26 20:55:20 +0200

    created a component for edit quote

---
+++ frontend/src/app/edit-quote/edit-quote.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-quote',
+  imports: [],
+  templateUrl: './edit-quote.html',
+  styleUrl: './edit-quote.css',
+})
+export class EditQuote {}
