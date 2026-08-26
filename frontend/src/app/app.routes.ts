6a8f60feaa1a8b6a1be97af972aaa95dbe61b9c8
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-26 20:58:12 +0200

    added routes for quote pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { EditBook } from './edit-book/edit-book';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { authGuard } from './guards/auth.guard';
+import { QuoteList } from './quote-list/quote-list';
+import { AddQuote } from './add-quote/add-quote';
+import { EditQuote } from './edit-quote/edit-quote';
 
 export const routes: Routes = [
     {
@@ -33,6 +36,21 @@ export const routes: Routes = [
     {
         path: 'books/edit/:id',
         component: EditBook,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'quotes',
+        component: QuoteList,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'quotes/new',
+        component: AddQuote,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'quotes/edit/:id',
+        component: EditQuote,
         canActivate: [authGuard]
     }
 ];
