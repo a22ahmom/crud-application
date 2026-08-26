5fa9124a43e036112d872835a75a85a196f65ac9
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-26 22:11:24 +0200

    added code to be able to edit a quote

--- frontend/src/app/edit-quote/edit-quote.ts
+++ frontend/src/app/edit-quote/edit-quote.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuoteService } from '../services/quote-service';
+import { Quote } from '../models/quote';
 
 @Component({
   selector: 'app-edit-quote',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './edit-quote.html',
   styleUrl: './edit-quote.css',
 })
-export class EditQuote {}
+export class EditQuote implements OnInit{
+
+  quoteId!: number;
+
+  text = '';
+  author = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private quoteService: QuoteService
+  ){}
+
+  ngOnInit() {
+
+    this.quoteId = Number(
+      this.route.snapshot.paramMap.get('id')
+    );
+
+    this.quoteService.getQuote(this.quoteId).subscribe({
+      next: quote => {
+        this.text = quote.text;
+        this.author = quote.author;
+      },
+
+      error: error => {
+        console.error('Kunde inte hämta citatet:', error);
+      }
+    });
+  }
+
+  saveQuote() {
+
+    const updatedQuote: Quote = {
+      id: this.quoteId,
+      text: this.text,
+      author: this.author
+    };
+
+    this.quoteService
+      .updateQuote(this.quoteId, updatedQuote)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/quotes']);
+        },
+
+        error: error => {
+          console.error(
+            'Kunde inte uppdatera citatet:',
+            error
+          );
+        }
+      });
+  }
+
+}
