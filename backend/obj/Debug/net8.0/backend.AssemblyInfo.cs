56b1b87b8b33cfdb2ca620c862dc6a5b8b2ba416
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-26 21:26:25 +0200

    added the code to be able to add a quote in the interface

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2a73e167bf89bc48208adfe06556e6c55aba9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c6a15f98f806ef5a4df0e898094fc029edb79b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/app/add-quote/add-quote.ts
+++ frontend/src/app/add-quote/add-quote.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { QuoteService } from '../services/quote-service';
+import { Quote } from '../models/quote';
 
 @Component({
   selector: 'app-add-quote',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-quote.html',
   styleUrl: './add-quote.css',
 })
-export class AddQuote {}
+export class AddQuote {
+
+  text = '';
+  author = '';
+
+  constructor(
+    private quoteService: QuoteService,
+    private router: Router
+  ){}
+
+  addQuote() {
+
+    const newQuote: Quote = {
+      text: this.text,
+      author: this.author
+    };
+
+    this.quoteService.addQuote(newQuote).subscribe({
+      next: () => {
+        this.router.navigate(['/quotes']);
+      },
+
+      error: error => {
+        console.error('Kunde inte lägga till citatet:', error);
+      }
+    });
+  }
+
+}
