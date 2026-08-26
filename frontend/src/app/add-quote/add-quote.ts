9ab678bf125cf9a9dc10a5dec5369cca64a917fd
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-26 20:54:38 +0200

    created a component for add quote

---
+++ frontend/src/app/add-quote/add-quote.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-quote',
+  imports: [],
+  templateUrl: './add-quote.html',
+  styleUrl: './add-quote.css',
+})
+export class AddQuote {}
