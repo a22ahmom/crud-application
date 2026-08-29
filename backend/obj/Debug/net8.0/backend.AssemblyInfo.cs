3f23e94ac5d952bb5fbbd96fe02553f20f2bf85a
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-29 15:55:22 +0200

    added a build for frontend and backend

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815d7a843f090a2d4988c2a03970704dbf04bff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac8941247268148db9ff19a8eb634acf46f22a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
