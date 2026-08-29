7e073f432c28ce8a2a3f9d0f17912b0af3bfefed
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-29 10:18:28 +0200

    add bootstrap in app.html which starts to look more appealing

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474d8fa63544aba59c7c57860fcaad7f6485fd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef17744df9c3d30a24e6e0ee0cfb8bc187688d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
