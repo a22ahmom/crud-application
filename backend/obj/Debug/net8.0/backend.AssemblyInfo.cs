474d8fa63544aba59c7c57860fcaad7f6485fd00
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-08-29 09:44:16 +0200

    installed bootstrap and fontawesome-free in angular locally

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95579d443a1cc0c9d0d4c51d84b2774550a3386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ba5332c2774692e0a34c321a9286f23e486a037")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
