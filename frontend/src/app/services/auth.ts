f8110d51bc34a699c08b85cdd99ecbb4fa221b49
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-09-01 09:34:38 +0200

    added a method to retrieve user Id

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -46,4 +46,23 @@ export class Auth {
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
   }
+
+  getCurrentUserId(): number | null {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      return null;
+    }
+
+    try {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+
+      const userId =
+        payload['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'];
+
+        return userId ? Number(userId) : null;
+    } catch {
+      return null;
+    }
+  }
 }
